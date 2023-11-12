87237ae33c36a49d9a5360a56e962c1e229475a1
Viswabaddala16 <122138862+Viswabaddala16@users.noreply.github.com>
2023-11-12 06:50:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 function threeSum(arr, target) {
-// write your code here
-  
+    let closestSum = Infinity;
+    let minDiff = Infinity;
+
+    for (let i = 0; i < arr.length - 2; i++) {
+        for (let j = i + 1; j < arr.length - 1; j++) {
+            for (let k = j + 1; k < arr.length; k++) {
+                const currentSum = nums[i] + nums[j] + nums[k];
+                const currentDiff = Math.abs(currentSum - target);
+
+                if (currentDiff < minDiff) {
+                    minDiff = currentDiff;
+                    closestSum = currentSum;
+                }
+            }
+        }
+    }
+
+    return closestSum;
 }
 
-module.exports = threeSum;
+modulfunctione.exports = threeSum;
+// Original code for reference
+
+
+// Example Usage:
